24e9be37d28dd3b9ce7a9a49a43ceb0898e39451
Gautam-2002 <gautam2sethi@gmail.com>
2022-08-08 18:39:53 +0530

    medium

---
+++ maximumSumCircularSubarray.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& arr) {
+    int kmax=INT_MIN,currsum1=0;
+    int totalsum=0,maxi=INT_MIN;
+    int kmin=INT_MAX,currsum2=0;
+    for (int i = 0; i < arr.size(); i++){
+        totalsum+=arr[i];
+
+        currsum1+=arr[i];
+        if(currsum1<0)currsum1=0;
+        kmax=max(kmax,currsum1);
+
+        currsum2+=arr[i];
+        if(currsum2>0)currsum2=0;
+        kmin=min(kmin,currsum2);
+
+        maxi=max(maxi,arr[i]);
+    }
+    int ans1=kmax;
+    int ans2=totalsum-kmin;
+    
+    if(ans1<=0 && ans2<=0)return maxi;
+    else if(ans1 > ans2)return ans1;
+    else return ans2;
+    }
+};
