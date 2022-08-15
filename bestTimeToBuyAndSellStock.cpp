f2ff61227f1712b12cf806dce7d8c33cb721afca
Gautam-2002 <gautam2sethi@gmail.com>
2022-08-15 22:44:44 +0530

    easy

---
+++ bestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans =0;
+        int mi=INT_MAX;
+        for(int i=0;i<prices.size();i++){
+            mi=min(mi,prices[i]);
+            ans=max(ans,prices[i]-mi);                      
+        }
+        return ans;
+    }
+};
