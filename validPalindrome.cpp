15e976aa4454a0e031bb0c0d525d2dac9915900d
Gautam-2002 <gautam2sethi@gmail.com>
2022-08-23 11:02:45 +0530

    easy

---
+++ validPalindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        transform(s.begin(),s.end(),s.begin(),::tolower);
+        s.erase(remove_if(s.begin(), s.end(), 
+[]( auto const& c ) -> bool { return !isalnum(c); } ), s.end());
+        int l=0,e=s.length()-1;
+        while(l<=e){
+            if(s[l]!=s[e])
+                return false;
+            l++;
+            e--;
+        }
+        return true;
+    }
+};
+// bool isPalindrome(string s) {
+//     for (int i = 0, j = s.size() - 1; i < j; i++, j--) { // Move 2 pointers from each end until they collide
+//         while (isalnum(s[i]) == false && i < j) i++; // Increment left pointer if not alphanumeric
+//         while (isalnum(s[j]) == false && i < j) j--; // Decrement right pointer if no alphanumeric
+//         if (toupper(s[i]) != toupper(s[j])) return false; // Exit and return error if not match
+//     }
+    
+//     return true;
+// }
