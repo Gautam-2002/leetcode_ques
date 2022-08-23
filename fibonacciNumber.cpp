7f309bd0df40f5c41c685d25dc0b1aa669824e03
Gautam-2002 <gautam2sethi@gmail.com>
2022-08-23 11:02:23 +0530

    easy

---
+++ fibonacciNumber.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+        int prev1=0;
+        int prev2=1;
+        if(n==0) return prev1;
+        for (int i = 2; i < n+1; i++)
+        {
+            int curr = prev1 + prev2;
+            prev1=prev2;
+            prev2=curr;
+        }
+        return prev2;
+    }
+};
