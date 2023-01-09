276445a75eb8f967c1ee1f1eeb0d27140b96bf22
Gautam_Sethi <84130499+Gautam-2002@users.noreply.github.com>
2023-01-09 11:51:02 +0530

    Time: 11 ms (92.41%), Space: 7 MB (73.85%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -7,42 +7,39 @@ public:
         int ans = 1;
         int st = 0,en = 0;
         
-        // if(n%2 != 0){
-            for(int i=0;i<n-1;++i){
-                int l = i,r = i;
-                while(l>=0 && r<n){
-                    if(s[l] == s[r]){
-                        l--;r++;
-                    }
-                    else
-                        break;
+        for(int i=0;i<n-1;++i){
+            int l = i,r = i;
+            while(l>=0 && r<n){
+                if(s[l] == s[r]){
+                    l--;r++;
                 }
-                int len = r-l-1;
-                if(len > ans){
-                    ans = len;
-                    st = l+1;
-                    en = r-1;
-                }
+                else
+                    break;
             }
-        // }
-        // else{
-            for(int i=0;i<n-1;++i){
-                int l = i,r = i+1;
-                while(l>=0 && r<n){
-                    if(s[l] == s[r]){
-                        l--;r++;
-                    }
-                    else
-                        break;
-                }
-                int len = r-l-1;
-                if(len > ans){
-                    ans = len;
-                    st = l+1;
-                    en = r-1;
-                }
+            int len = r-l-1;
+            if(len > ans){
+                ans = len;
+                st = l+1;
+                en = r-1;
             }
-        // }
+        }
+
+        for(int i=0;i<n-1;++i){
+            int l = i,r = i+1;
+            while(l>=0 && r<n){
+                if(s[l] == s[r]){
+                    l--;r++;
+                }
+                else
+                    break;
+            }
+            int len = r-l-1;
+            if(len > ans){
+                ans = len;
+                st = l+1;
+                en = r-1;
+            }
+        }
         return s.substr(st,ans);
     }
 };
