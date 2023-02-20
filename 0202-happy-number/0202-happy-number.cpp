8b6453ee7f909989aea61295edbd907629a420ee
Gautam_Sethi <84130499+Gautam-2002@users.noreply.github.com>
2023-02-20 19:18:42 +0530

    Time: 2 ms (58.75%), Space: 6.2 MB (41.95%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_map<int,int> mp;
+        int x = n;
+        int a = 0; 
+        while(mp.find(x)==mp.end() || mp[x]<=1){
+            string s = to_string(x);
+            for(auto i:s){
+                int digit = (i-'0');
+                a += (digit*digit);
+            }
+            if(a==1)
+                return true;
+            mp[a]++;
+            x = a;
+            a = 0;
+        }
+        cout<<x<<" ";
+        return false;
+        
+    }
+};
