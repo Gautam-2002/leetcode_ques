517f779980b3c716993fb04d1e29f33ba92acfef
Gautam_Sethi <84130499+Gautam-2002@users.noreply.github.com>
2023-04-20 13:43:44 +0530

    Time: 28 ms (60.12%), Space: 10 MB (55.63%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void solve(int n,int k,vector<int> &v,int i){
+        // if(i>n || k<=0){
+        //     if(k==0)
+        //         ans.push_back(v);
+        //     return;
+        // }
+        // solve(n,k,v,i+1);
+        // v.push_back(i);
+        // solve(n,k-1,v,i+1);
+        if(v.size() == k){
+            ans.push_back(v);
+            return;
+        }        
+        for(int j=i;j<n+1;j++){
+            v.push_back(j);
+            solve(n,k,v,j+1);
+            v.pop_back();
+        }
+     }
+    
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> v;
+        solve(n,k,v,1);
+        return ans;
+    }
+};
