e28293bf041657578f7c7b2bf32a809137edca13
Gautam_Sethi <84130499+Gautam-2002@users.noreply.github.com>
2023-04-24 22:05:48 +0530

    Time: 12 ms (45.30%), Space: 6 MB (69.26%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
-    int dp[31];
-    
-    int fib(int n) {
-        if(n==0 || n==1){
+    int solve(int n){
+        if(n==0 || n==1)
             return n;
-        }
-        memset(dp,-1,sizeof(dp));
         
-        if(dp[n] != -1){
-            return dp[n];
-        }
-        
-        return dp[n] = fib(n-1) + fib(n-2);
+        return solve(n-1)+solve(n-2);
+    }
+    int fib(int n) {
+        // vector<int> dp(n+1,-1);
+        return solve(n);
     }
 };
