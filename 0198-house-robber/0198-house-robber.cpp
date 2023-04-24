81c5f8012504f8273d0f5b4c03034e0da270eb2e
Gautam_Sethi <84130499+Gautam-2002@users.noreply.github.com>
2023-04-24 22:21:59 +0530

    Time: 5 ms (25.96%), Space: 7.9 MB (36.06%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
+    int solve(vector<int>& nums,int n,vector<int> &dp){
+        if(n>=nums.size())
+            return 0;
+        
+        if(dp[n]!=-1)return dp[n];
+        
+        int in = nums[n] + solve(nums,n+2,dp);
+        int ex = solve(nums,n+1,dp);
+        
+        return dp[n]=max(in,ex);
+    }
     int rob(vector<int>& nums) {
-        int n = nums.size(), pre = 0, cur = 0;
-        for (int i = 0; i < n; i++) {
-            int temp = max(pre + nums[i], cur);
-            pre = cur;
-            cur = temp;
-        }
-        return cur;
+        vector<int> dp(nums.size()+1,-1);
+        return solve(nums,0,dp);
     }
 };
