6b732479f9846bb63f192f0401720615da73a4c2
Gautam Sethi <84130499+Gautam-2002@users.noreply.github.com>
2023-05-13 14:46:19 +0530

    Time: 353 ms (72.25%), Space: 108.1 MB (10.47%) - LeetHub

--- 2623-memoize/2623-memoize.js
+++ 2623-memoize/2623-memoize.js
@@ -1,21 +1,32 @@
 /**
  * @param {Function} fn
  */
+// function memoize(fn) {
+//     let arr = {};
+//     return function(...args) {
+//         let arg = JSON.stringify(args);
+//         // for(let i=0;i<arr.length;i++){
+//         //     let {key,val} = arr[i];
+//         //     if(key===arg)
+//         //         return val;
+//         // }
+//         if(arg in arr)
+//             return arr[arg];
+//         let ans = fn(...args);
+//         // arr.push({arg:ans})
+//         arr[arg] = ans;
+//         return ans;
+//     }
+// }
 function memoize(fn) {
-    let arr = {};
+    const cache = new Map();
     return function(...args) {
-        let arg = JSON.stringify(args);
-        // for(let i=0;i<arr.length;i++){
-        //     let {key,val} = arr[i];
-        //     if(key===arg)
-        //         return val;
-        // }
-        if(arg in arr)
-            return arr[arg];
-        let ans = fn(...args);
-        // arr.push({arg:ans})
-        arr[arg] = ans;
-        return ans;
+        if(cache.has(JSON.stringify(args))){
+            return cache.get(JSON.stringify(args));
+        }
+        const res = fn(...args);
+        cache.set(JSON.stringify(args),res);
+        return res;
     }
 }
 
