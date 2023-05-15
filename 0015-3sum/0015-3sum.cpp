68069258a8379cd80d7586b1d5dffce951ef67f9
Gautam Sethi <84130499+Gautam-2002@users.noreply.github.com>
2023-05-15 06:20:53 +0530

    Time: 190 ms (61.37%), Space: 25.9 MB (55.13%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -4,7 +4,7 @@ public:
         sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
         int n = nums.size();
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n-2;i++){
             if(i > 0 && nums[i]==nums[i-1])
                 continue;
             int j=i+1;
