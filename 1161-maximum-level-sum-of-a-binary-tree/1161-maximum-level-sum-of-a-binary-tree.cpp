835fbc71f4e046ac7e692c05f9ee2c22b17fd3f0
Gautam Sethi <84130499+Gautam-2002@users.noreply.github.com>
2023-06-15 18:10:18 +0530

    Time: 216 ms (29.67%), Space: 107.3 MB (24.55%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -44,3 +44,75 @@ public:
         return res.second;
     }
 };
+
+// class Solution {
+// public:
+//     void dfs(TreeNode* node, int level, vector<int>& sumOfNodesAtLevel) {
+//         if (node == nullptr) {
+//             return;
+//         }
+
+//         if (sumOfNodesAtLevel.size() == level) {
+//             sumOfNodesAtLevel.push_back(node->val);
+//         } else {
+//             sumOfNodesAtLevel[level] += node->val;
+//         }
+
+//         dfs(node->left, level + 1, sumOfNodesAtLevel);
+//         dfs(node->right, level + 1, sumOfNodesAtLevel);
+//     }
+
+//     int maxLevelSum(TreeNode* root) {
+//         vector<int> sumOfNodesAtLevel;
+//         dfs(root, 0, sumOfNodesAtLevel);
+
+//         int maxSum = INT_MIN;
+//         int ans = 0;
+
+//         for (int i = 0; i < sumOfNodesAtLevel.size(); i++) {
+//             if (maxSum < sumOfNodesAtLevel[i]) {
+//                 maxSum = sumOfNodesAtLevel[i];
+//                 ans = i + 1;
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
+
+
+// class Solution {
+// public:
+//     int maxLevelSum(TreeNode* root) {
+//         int maxSum = INT_MIN;
+//         int ans = 0, level = 0;
+
+//         queue<TreeNode*> q;
+//         q.push(root);
+
+//         while (!q.empty()) {
+//             level++;
+//             int sumAtCurrentLevel = 0;
+//             // Iterate over all the nodes in the current level.
+//             for (int sz = q.size(); sz > 0; --sz) {
+//                 TreeNode* node = q.front();
+//                 q.pop();
+//                 sumAtCurrentLevel += node->val;
+
+//                 if (node->left != nullptr) {
+//                     q.push(node->left);
+//                 }
+//                 if (node->right != nullptr) {
+//                     q.push(node->right);
+//                 }
+//             }
+
+//             if (maxSum < sumAtCurrentLevel) {
+//                 maxSum = sumAtCurrentLevel;
+//                 ans = level;
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
