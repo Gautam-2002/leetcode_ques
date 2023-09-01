f02361fae242adcad095cbe9e3e89431e5cdc935
Gautam Sethi <84130499+Gautam-2002@users.noreply.github.com>
2023-09-01 22:12:25 +0530

    Time: 6 ms (56.68%), Space: 7.8 MB (89.73%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -3,13 +3,18 @@ public:
     vector<int> countBits(int n) {
         vector<int> ans(n+1);
         for(int i=0;i<=n;i++){
-            string str = bitset<32>(i).to_string();
-            int x=0;
-            for(auto s:str){
-                if(s=='1')
-                    x++;
-            }
-            ans[i] = x;
+            // string str = bitset<32>(i).to_string();
+            // int x=0;
+            // for(auto s:str){
+            //     if(s=='1')
+            //         x++;
+            // }
+            // ans[i] = x;
+            
+            // or :
+            
+            ans[i] = __builtin_popcount(i);
+            
         }
         return ans;
     }
