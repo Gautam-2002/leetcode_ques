e33685b063a894471fb48eac0ec843dc71a960ac
Gautam Sethi <84130499+Gautam-2002@users.noreply.github.com>
2023-09-01 22:07:07 +0530

    Time: 23 ms (6.06%), Space: 14.7 MB (5.98%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1);
+        for(int i=0;i<=n;i++){
+            string str = bitset<32>(i).to_string();
+            int x=0;
+            for(auto s:str){
+                if(s=='1')
+                    x++;
+            }
+            ans[i] = x;
+        }
+        return ans;
+    }
+};
