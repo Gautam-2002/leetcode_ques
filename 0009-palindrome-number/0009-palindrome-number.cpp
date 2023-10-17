3403ca9b1f47f73f44e6e3979e2c0ec6fc14dd72
Gautam Sethi <84130499+Gautam-2002@users.noreply.github.com>
2023-10-17 23:00:56 +0530

    Time: 11 ms (44.42%), Space: 6.3 MB (41.58%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string s1 = to_string(x);
-        string s2 = s1;
-        reverse(s1.begin(),s1.end());
-        return s1==s2;
+        if(x==0)return true;
+        if((x<0) || (x%10==0))return false;
+        int y = x;
+        long long z = 0;
+        while(y>0){
+            z = (z*10)+(y%10);
+            y/=10;
+        }
+        return z==x;
     }
 };
