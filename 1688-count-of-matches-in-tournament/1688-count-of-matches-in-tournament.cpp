b1981a70bc8cd953500c8bff692b0ab61869482b
Gautam Sethi <84130499+Gautam-2002@users.noreply.github.com>
2023-12-05 23:24:58 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (36.15%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -1,20 +1,20 @@
-// class Solution {
-// public:
-//     int numberOfMatches(int n) {
-//         if(n == 1)return 0;
-//         if(n == 2)return 1;
-        
-//         if(n & 1)
-//             return (n/2) + numberOfMatches(n/2+1);
-//         else
-//             return (n/2) + numberOfMatches(n/2);
-//     }
-// };
-// from solutions : 
-// Intution each match eliminated 1 team to to eliminate n-1 teams it will take n-1 matches (as 1 team will we winner at the end)
 class Solution {
 public:
     int numberOfMatches(int n) {
-        return n - 1;
+        if(n == 1)return 0;
+        if(n == 2)return 1;
+        
+        if(n & 1)
+            return (n/2) + numberOfMatches(n/2+1);
+        else
+            return (n/2) + numberOfMatches(n/2);
     }
 };
+// from solutions : 
+// Intution each match eliminated 1 team to to eliminate n-1 teams it will take n-1 matches (as 1 team will we winner at the end)
+// class Solution {
+// public:
+//     int numberOfMatches(int n) {
+//         return n - 1;
+//     }
+// };
