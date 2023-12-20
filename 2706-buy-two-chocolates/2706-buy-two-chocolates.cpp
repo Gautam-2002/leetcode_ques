3ba12a754b5bdb35ab5022042a359fa798284960
Gautam Sethi <84130499+Gautam-2002@users.noreply.github.com>
2023-12-20 21:57:58 +0530

    Time: 8 ms (81.59%), Space: 47 MB (58.30%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        int a=INT_MAX,b=INT_MAX,c=INT_MAX;
+        for(auto i:prices){
+            if(a>i){
+                c = min(c,a);
+                a = i;
+            }
+            else if(b>i){
+                b = min(c,i);
+            }
+            // cout<<a<<" "<<b<<" "<<c<<endl;
+        }
+        b = min(b,c);
+        int x = (money-a-b);
+        if(x >=0 ) return x;
+        else return money;
+    }
+};
